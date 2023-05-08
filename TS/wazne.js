7ef71b572caad7f89a0119a843c2b0f89d5331fe
micro <microfalowka@gmail.com>
2023-05-08 19:19:56 +0200

    Master

---
+++ TS/wazne.js
@@ -0,0 +1,7 @@
+//npm i -g typescript  //trzeba paczke
+// tsc --version
+
+// npm i -D ts-node  //narzedzie deweloperskie zeby odpalic w ts
+
+// ale mozliwe ze trzeba npm i -g ts-node
+//ts-node index.ts
