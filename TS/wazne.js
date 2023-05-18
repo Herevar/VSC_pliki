b0d957a7cdce8ebb29bdc49c2f4f0adcd33da790
micro <microfalowka@gmail.com>
2023-05-18 19:31:45 +0200

    asd

--- TS/wazne.js
+++ TS/wazne.js
@@ -1,8 +1,10 @@
 //npm i -g typescript  //trzeba paczke
 // tsc --version
 
-// npm i -D ts-node  //narzedzie deweloperskie zeby odpalic w ts
-
+// npm i -D ts-node  //narzedzie deweloperskie zeby w ogole odpalic w ts, czyli jak uruchamiamy ts-node xxxxx.ts
+// npm i -D ts-node-Dev    // jak nodemon -> czyli na bierzaco wczytuje zmiany ; uruchamiamy to : tsnd xxx.js
+    //oczywiscie w json nie moze zrobic "start:" ts-node xxxx.ts
+    //                                  "start:dev:" tsnd xxxx.ts    // no i pozniej npm start itd   
 // ale mozliwe ze trzeba npm i -g ts-node  (jezeli ts-node normalnie nie zadziała to globalnie trzeba odpalanie tego)
 //ts-node index.ts
 
