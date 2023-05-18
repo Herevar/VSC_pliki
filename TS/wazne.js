d56d24aebb039b503276c3ee062e03872ed611dd
micro <microfalowka@gmail.com>
2023-05-18 19:08:22 +0200

    Master pulluj kurwa

--- TS/wazne.js
+++ TS/wazne.js
@@ -3,5 +3,22 @@
 
 // npm i -D ts-node  //narzedzie deweloperskie zeby odpalic w ts
 
-// ale mozliwe ze trzeba npm i -g ts-node
+// ale mozliwe ze trzeba npm i -g ts-node  (jezeli ts-node normalnie nie zadziała to globalnie trzeba odpalanie tego)
 //ts-node index.ts
+
+// trzeba dodac w task.json
+// {
+//     "version": "2.0.0",
+//     "tasks": [
+//         {
+//             "label": "typescript",
+//             "type": "typescript",
+//             "tsconfig": "tsconfig.json",
+//             "option": "watch",
+//             "auto": true,
+//             "problemMatcher": [
+//                 "$tsc-watch"
+//             ]
+//         }
+//     ]
+// }
